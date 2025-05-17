1fadbac4a08dca9bd7d823d3a412e4df598c3c86
Lĩnh <21130428@st.hcmuaf.edu.vn>
2025-05-17 23:31:58 +0700

    add RegisterPage.css for RegisterPage.js

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,23 +1,9 @@
 import React, { useState } from 'react';
 // import axios from 'axios';
-import "./RegisterPage.scss"
+import "./RegisterPage.css"
 
 const RegisterPage = () => {
-    // const [username, setUsername] = useState('');
-    // const [password, setPassword] = useState('');
 
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const newUser = { username, password };
-    //         await axios.post('http://localhost:3000/users', newUser); // Gửi POST request đến db.json
-    //         alert('Đăng kí thành công!');
-    //         // Sau khi đăng kí thành công, có thể chuyển hướng hoặc thực hiện các xử lý khác
-    //     } catch (error) {
-    //         console.error('Lỗi khi đăng kí:', error);
-    //         alert('Đăng kí thất bại! Vui lòng thử lại.');
-    //     }
-    // };
 
     return (
         <div className="register-page">
